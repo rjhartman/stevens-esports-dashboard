6d23761f45df7e9a045053bcdc1eca60964fea05
dpekata <dpekata@stevens.edu>
2021-05-03 18:57:16 -0400

    Added a mock team database, updated the overall teams page, made individual teams pages

---
+++ data/teams.js
@@ -0,0 +1,26 @@
+module.exports = [
+    {
+        _id: 0,
+        name: "Stevens League of Legends Red",
+        status: "Varsity",
+        game: "League of Legends",
+        players: ["Jerry Cheng", 
+        "Andrew Chuah"],
+    },
+    {
+        _id: 1,
+        name: "Stevens Overwatch Gray",
+        status: "Junior Varsity",
+        game: "Overwatch",
+        players: ["Jerry Chen", 
+        "Daniel Pekata"],
+    },
+    {
+        _id: 2,
+        name: "Stevens Counter Strike Red",
+        status: "Varsity",
+        game: "Cunter Strike",
+        players: ["Ryan Hartman", 
+        "Patrick Hill"],
+    },
+]
