a517830c62af78d0b54c6b4c55a88c6b6e091b64
Andrew Chuah <warlordpluto@gmail.com>
2021-05-09 22:01:12 -0400

    moved header/footer to main handlebars

--- public/js/client.js
+++ public/js/client.js
@@ -2,6 +2,29 @@ function dropdown() {
     document.getElementById("team-list").classList.toggle("show");
 }
 
+$(document).ready(function () {
+    $(".resolved_matches").css("display","none");
+    $(".unresolved_matches").css("display","block");
+    // alert('hi');
+    
+});
+$("#results").click(function(event){
+    $(".resolved_matches").show();
+    $("#results").css("background-color", "rgb(204, 29, 29)");
+    $("#results").css("color", "white");
+    $("#upcoming").css("background-color", "white");
+    $("#upcoming").css("color", "rgb(204, 29, 29)");
+    $(".unresolved_matches").hide();
+});
+$("#upcoming").click(function(event){
+    $(".resolved_matches").hide();
+    $("#upcoming").css("background-color", "rgb(204, 29, 29)");
+    $("#upcoming").css("color", "white");
+    $("#results").css("background-color", "white");
+    $("#results").css("color", "rgb(204, 29, 29)");
+    $(".unresolved_matches").show();
+});
+
 window.onclick = function(e) {
     if(!e.target.matches('.dropdownButton')){
         let dropdown = document.getElementById("team-list");
@@ -9,18 +32,3 @@ window.onclick = function(e) {
             dropdown.classList.remove("show");
     }
 }
-
-$(document).ready(function () {
-    $(".resolved_matches").css("display","none");
-    $(".unresolved_matches").css("display","block");
-    // alert('hi');
-    
-});
-$(".results").click(function(event){
-    $(".resolved_matches").css("display","block");
-    $(".unresolved_matches").css("display","none");
-});
-$(".upcoming").click(function(event){
-    $(".resolved_matches").css("display","none");
-    $(".unresolved_matches").css("display","block");
-});
