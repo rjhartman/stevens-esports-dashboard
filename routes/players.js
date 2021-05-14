23eba00b75495914dfaafa74164139ff0c4725b5
Jerrytd579 <jerrytd579@gmail.com>
2021-05-13 23:36:21 -0400

    more redirects - if error, simply redirect to homepage

--- routes/players.js
+++ routes/players.js
@@ -13,16 +13,11 @@ router.get('/', async (req, res) => {
 });
 
 router.get('/:playerid', async (req, res) => {
-    if(req.params.playerid == undefined){
-        res.status(400).render('player/playerNotExist', {title: 'Error 404: Player not Found!'});
+    if(req.params.playerid == undefined || typeof req.params.playerid != 'string'){
+        res.redirect('/');
+        //res.status(400).render('player/playerNotExist', {title: 'Error 404: Player not Found!'});
         return;
     }
-
-    if(typeof req.params.playerid != 'string'){
-        res.status(400).render('player/error', {title: '400', error: 'Code 400: Search term is not type string.'});
-        return;
-    }
-
 
     try {
         let id = req.params.playerid;
@@ -30,7 +25,7 @@ router.get('/:playerid', async (req, res) => {
         res.render('pages/profile', {title: person.nickname + " | Stevens Esports", player: person});
     } catch (e) {
         //res.status(500).render('shows/errorpage', {title: 'Error', error: e });
-        res.status(500);
+        res.redirect('/');
         console.log(e);
     }
 });
