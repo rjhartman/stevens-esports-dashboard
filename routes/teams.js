d2cf856d82ef660ca7f849f2b3b385c5c73629c5
dpekata <dpekata@stevens.edu>
2021-05-13 23:57:06 -0400

    messed up installing mongo lol

--- routes/teams.js
+++ routes/teams.js
@@ -1,16 +1,18 @@
 const express = require('express');
 const router = express.Router();
 const data = require('../data');
-// const teamData = data.teams;
+const teamData = data.teams;
 const teamFuncs = data.teamfunctions;
 // const playerData = data.players;
 const mongoCollections = require('../config/mongoCollections');
 const teams = mongoCollections.teams;
+const players = mongoCollections.players;
 let { ObjectId } = require('mongodb');
 
 router.get('/', async (req, res) => {
     try {
         const teamCollection = await teams();
+        // res.render('teams/teamslist', {title: 'List of Teams', teams: teamData});
         res.render('teams/teamslist', {title: 'List of Teams', teams: teamCollection});
     } catch (e) {
         res.status(500);
@@ -22,18 +24,20 @@ router.get('/:id', async (req, res) => {
         players = []
         let parsedId = ObjectId(req.params.id);
         let id = req.params.id;
-        const teamCollection = await teams();
+        // console.log(id)
+        // const teamCollection = await teams();
+        // const playerCollection = await players();
         const team = await teamFuncs.getTeamById(id);
         // this is so we can eventually pass in both players and teams from the database
-        for (i = 0; i < playerData.length; i++) {
-            for (j = 0; j < team.players.length; j++) {
-                if (playerData[i]._id == team.players[i]._id) {
-                    players.push(playerData[i].name)
-                }
-            }
-        }
-        res.render('teams/teampage', {title: team.name, team: team, players: players });
-        // res.render('teams/teampage', {title: team.name, team: team });
+        // for (i = 0; i < playerData.length; i++) {
+        //     for (j = 0; j < team.players.length; j++) {
+        //         if (playerData[i]._id == team.players[i]._id) {
+        //             players.push(playerData[i].name)
+        //         }
+        //     }
+        // }
+        res.render('teams/teampage', {title: team.name, team: team});
+        // res.render('teams/teampage', {title: team.name, team: team, players: players });
     } catch (e) {
         //res.status(500).render('shows/errorpage', {title: 'Error', error: e });
         res.status(500);
@@ -43,3 +47,4 @@ router.get('/:id', async (req, res) => {
 
 
 module.exports = router;
+
