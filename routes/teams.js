a26ee76c62dcfe84728015be8048ef205e767fcc
dpekata <dpekata@stevens.edu>
2021-05-15 15:34:38 -0400

    removed route we dont need

--- routes/teams.js
+++ routes/teams.js
@@ -64,39 +64,6 @@ router.get('/', async (req, res) => {
     }
 });
 
-// router.get('/:id', async (req, res) => {
-//     try {
-//         players_array = []
-//         let parsedId = ObjectId(req.params.id);
-//         let id = req.params.id;
-//         // console.log(id)
-//         // const teamCollection = await teams();
-//         const playerCollection = await players();
-//         const team = await teamFuncs.getTeamById(id);
-//         // this is so we can eventually pass in both players and teams from the database
-//         for (i = 0; i < playerCollection.length; i++) {
-//             let player_i = await player.getPlayerById(playerCollection[i]._id);
-//             for (j = 0; j < team.players.length; j++) {
-//                 if (player_i._id == team.players[j]._id) {
-//                     players_array.push[player_i.user]
-//                 }
-//             }
-//         }
-//         // for (i = 0; i < playerCollection.length; i++) {
-//         //     for (j = 0; j < team.players.length; j++) {
-//         //         if (playerCollection[i]._id == team.players[j]._id) {
-//         //             players.push(playerCollection[i].name)
-//         //         }
-//         //     }
-//         // }
-//         // res.render('teams/teampage', {title: team.name, team: team});
-//         res.render('teams/teampage', {title: team.name, team: team, players: players_array });
-//     } catch (e) {
-//         res.status(500);
-//         console.log(e);
-//     }
-// });
-
 router.put('/:id', async (req, res) => {
     xss(req.body.name);
     xss(req.body.description);
