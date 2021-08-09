583449d25ae3236979da91544df3f4f91c6d1eb3
Andrew Chuah <warlordpluto@gmail.com>
2021-08-09 18:07:25 -0400

    added popup window for deleting account

--- public/js/client.js
+++ public/js/client.js
@@ -1,7 +1,3 @@
-function dropdown() {
-    document.getElementById("team-list").classList.toggle("show");
-}
-
 $(document).ready(function () {
     $(".resolved_matches").css("display","none");
     $(".unresolved_matches").css("display","block");
@@ -24,3 +20,22 @@ $("#upcoming").click(function(event){
     $("#results").css("color", "rgb(204, 29, 29)");
     $(".unresolved_matches").show();
 });
+
+
+// Pop-up window
+var modal = document.getElementById("deleteWindow");
+var btn = document.getElementById("delete");
+var span = document.getElementsByClassName("close")[0];
+
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+    if(event.target == modal)
+        modal.style.display = "none";
+}
