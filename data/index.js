8f5d16ebdce7f559d6efa24785387e6776e7531a
Sheathblade <warlordpluto@gmail.com>
2021-08-15 01:06:44 -0400

    commented out players in index

--- data/index.js
+++ data/index.js
@@ -1,12 +1,12 @@
 const usersData = require("./users");
-const playerData = require("./players");
+//const playerData = require("./players");
 const matchData = require("./match");
 const gameData = require("./game");
 const teamData = require("./teamfunctions");
 
 module.exports = {
   users: usersData,
-  players: playerData,
+  //players: playerData,
   match: matchData,
   teams: teamData,
   game: gameData
